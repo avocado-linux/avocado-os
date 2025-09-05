75b827a5b005fcf12803215609f68dfdb6103772
Justin Schneck <j.schneck@peridio.com>
2025-09-05 01:58:48 -0400

    Update elixir reference example

--- references/elixir/elixir-compile.sh
+++ references/elixir/elixir-compile.sh
@@ -1,5 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
 echo "Compiling Elixir application"
 cd ref-elixir
+export MIX_ENV=prod
 mix deps.get
-mix compile 
+mix assets.setup
+mix compile
+mix assets.deploy
 mix release --overwrite
